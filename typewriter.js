ae2d31ec2ea65e638b2898e1f196c622b56f0393
‘Charles <charlesthompson.2693@gmail.com>
2020-09-22 20:18:23 +0000

    set timeout for typed animation

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let counter = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, counter);
+  counter += 50;
+}
+setTimeout(() => {
+  process.stdout.write("\n");
+}, counter);
